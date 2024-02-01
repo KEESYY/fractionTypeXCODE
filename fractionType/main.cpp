68a6f65b2dbf14ac16b1f3b2e2de9e5ca8733864
keescode <markeesjohnson13@oulook.com>
2024-02-01 08:57:47 -0600

    Initial Commit

---
+++ fractionType/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  fractionType
+//
+//  Created by Markees Johnson on 2/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
